bd070ca65c9ff989a5f21ab5262b75dca7d7ff12
Rafael Shen <rafaelshen977@gmail.com>
2019-03-04 16:34:28 +0800

    Add more array generation func

--- main.go
+++ main.go
@@ -16,32 +16,59 @@ func main() {
 		size, _ = strconv.Atoi(os.Args[1])
 	}
 
-	arr := generateArray(size)
+	// arr := genPerm(size)
+	arr := genRand(size)
+	// arr := genSeq(size)
+	// arr := genRev(size)
 
 	// Quicksort
 	arr1 := make([]int, len(arr))
 	copy(arr1, arr)
 	start := time.Now()
-	// fmt.Println("Array before sorting", arr)
+	// fmt.Println("Array before sorting", arr1)
 	sort.QuickSort(arr1)
-	// fmt.Println("Array after sorting", arr)
+	// fmt.Println("Array after sorting", arr1)
 	fmt.Printf("Execution time for quicksort %s\n", time.Since(start))
 
-	// 2 pivot Quicksort
+	// // 2 pivot Quicksort
 	arr2 := make([]int, len(arr))
 	copy(arr2, arr)
 	start = time.Now()
 	sort.QuickSort2P(arr2)
 	fmt.Printf("Execution time for 2 pivot quicksort %s\n", time.Since(start))
 
-	// 3 pivot Quicksort
+	// // 3 pivot Quicksort
 	arr3 := make([]int, len(arr))
 	copy(arr3, arr)
 	start = time.Now()
 	sort.QuickSort3P(arr3)
-	fmt.Printf("Execution time for 3 pivot quicksort %s\n", time.Since(start))
+	fmt.Printf("Execution time for 3 pivot quicksort %v\n", time.Since(start))
 }
 
-func generateArray(n int) []int {
+func genPerm(n int) []int {
 	return rand.Perm(n)
+}
+
+func genRand(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = rand.Intn(100)
+	}
+	return arr
+}
+
+func genSeq(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = i
+	}
+	return arr
+}
+
+func genRev(n int) []int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = n - i - 1
+	}
+	return arr
 }
