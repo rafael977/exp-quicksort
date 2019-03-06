41dd297edc6d1a521419e569f570b42b93e3750d
Rafael Shen <rafael.shen977@gmail.com>
2019-03-06 10:51:15 +0800

    Tidy up

--- main.go
+++ main.go
@@ -21,39 +21,21 @@ func main() {
 	// arr := sort.GenRev(size)
 
 	// Quicksort
-	arr1 := make([]int, len(arr))
-	copy(arr1, arr)
-	start := time.Now()
-	// fmt.Println("Array before sorting", arr1)
-	sort.QuickSort(arr1)
-	// fmt.Println("Array after sorting", arr1)
-	fmt.Printf("Execution time for quicksort %s\n", time.Since(start))
-
+	runSort(sort.QuickSort, 1, arr)
 	// 2 pivot Quicksort
-	arr2 := make([]int, len(arr))
-	copy(arr2, arr)
-	start = time.Now()
-	sort.QuickSort2P(arr2)
-	fmt.Printf("Execution time for 2 pivot quicksort %s\n", time.Since(start))
-
+	runSort(sort.QuickSort2P, 2, arr)
 	// 3 pivot Quicksort
-	arr3 := make([]int, len(arr))
-	copy(arr3, arr)
-	start = time.Now()
-	sort.QuickSort3P(arr3)
-	fmt.Printf("Execution time for 3 pivot quicksort %v\n", time.Since(start))
-
+	runSort(sort.QuickSort3P, 3, arr)
 	// 5 pivot Quicksort
-	arr5 := make([]int, len(arr))
-	copy(arr5, arr)
-	start = time.Now()
-	sort.QuickSort5P(arr5)
-	fmt.Printf("Execution time for 5 pivot quicksort %v\n", time.Since(start))
-
+	runSort(sort.QuickSort5P, 5, arr)
 	// 7 pivot Quicksort
-	arr7 := make([]int, len(arr))
-	copy(arr7, arr)
-	start = time.Now()
-	sort.QuickSort7P(arr7)
-	fmt.Printf("Execution time for 7 pivot quicksort %v\n", time.Since(start))
+	runSort(sort.QuickSort7P, 7, arr)
+}
+
+func runSort(fn func([]int), numPivots int, arr []int) {
+	cpy := make([]int, len(arr))
+	copy(cpy, arr)
+	start := time.Now()
+	fn(cpy)
+	fmt.Printf("Execution time for %d pivot quicksort %.3fms\n", numPivots, time.Since(start).Seconds()*1000)
 }
--- pkg/sort/gen.go
+++ pkg/sort/gen.go
@@ -1,14 +1,19 @@
 package sort
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // GenPerm generates permutation of n
 func GenPerm(n int) []int {
+	rand.Seed(time.Now().UnixNano())
 	return rand.Perm(n)
 }
 
 // GenRand generates array of random int of size n
 func GenRand(n int) []int {
+	rand.Seed(time.Now().UnixNano())
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
 		arr[i] = rand.Intn(100)
@@ -18,6 +23,7 @@ func GenRand(n int) []int {
 
 // GenSeq generates sorted int array of size n
 func GenSeq(n int) []int {
+	rand.Seed(time.Now().UnixNano())
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
 		arr[i] = i
@@ -27,6 +33,7 @@ func GenSeq(n int) []int {
 
 // GenRev generates reversed int array of size n
 func GenRev(n int) []int {
+	rand.Seed(time.Now().UnixNano())
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
 		arr[i] = n - i - 1
